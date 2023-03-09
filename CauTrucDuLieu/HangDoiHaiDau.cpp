8cf00e9b5d54d32ec6a693a8ebf959f83dc2d59e
NgocJQK <minhngocyn1@gmail.com>
2023-03-09 13:22:31 +0700

    update hàng đợi hai đầu

---
+++ CauTrucDuLieu/HangDoiHaiDau.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <deque>
+
+int main() {
+    std::deque<int> mydeque;
+
+    // Thêm phần tử vào đầu deque
+    mydeque.push_front(10);
+    mydeque.push_front(20);
+
+    // Thêm phần tử vào cuối deque
+    mydeque.push_back(30);
+    mydeque.push_back(40);
+
+    // In ra các phần tử trong deque
+    for (auto it = mydeque.begin(); it != mydeque.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    // Output: 20 10 30 40
+
+    // Xóa phần tử đầu tiên của deque
+    mydeque.pop_front();
+
+    // Xóa phần tử cuối cùng của deque
+    mydeque.pop_back();
+
+    // In ra các phần tử trong deque sau khi xóa
+    for (auto it = mydeque.begin(); it != mydeque.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    // Output: 10 30
+    // trả về phần tử đầu tiên của deque
+    std::cout << mydeque.front() << std::endl;
+    // trả về phần tử cuối cùng của deque
+    std::cout << mydeque.back() << std::endl;
+    // kiểm tra deque có rỗng hay không
+    bool check = mydeque.empty();
+    // trả về số phần tử trong deque
+    int size = mydeque.size();
+    // xóa toàn bộ phần tử trong deque
+    mydeque.clear();
+    // Xóa toàn bộ không tử trong deque
+    mydeque.erase(mydeque.begin(), mydeque.end());
+    
+
+
+    return 0;
+}
